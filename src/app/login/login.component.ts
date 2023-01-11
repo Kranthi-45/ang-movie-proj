e7ef6a14da05810530e264ad0f9a29622372d1dd
Kranthi-45 <kranthikumarbolli@gmail.com>
2023-01-11 17:43:22 +0530

    buttons, loader , mobile view adjustments

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
   unamePattern = "[a-zA-Z0-9]+";
   userDetails: any;
   isLoading: boolean = false;
-
+  errorMessage:String = "";
   constructor(private fb: FormBuilder, private router: Router, private ls: LoginService) {
     this.loginForm = this.fb.group({
       username: ['', [Validators.required, Validators.maxLength(15)]],
@@ -70,9 +70,11 @@ export class LoginComponent implements OnInit {
       console.log("---------------Invalid Credentials------------");
       if (!error.error.is_success && error.error.error.code === "invalid_login_credentials") {
         console.log("User Password is Incorrect...");
+        this.errorMessage = "Entered Password is Incorrect."
         this.status = true;
       } else {
         console.log("User doesn't exits in our database.");
+        this.errorMessage = "Entered User doesn't exists in our Database."
         this.status = true;
       }
       console.log(error);
